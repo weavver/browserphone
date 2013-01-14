ad2eeb9e30e920513fe5fb1b039a86383acf9e23
Mitchel Constantin <mythicalbox@weavver.com>
2013-01-14 15:12:30 -0800

    folder restructure

--- BuildWeavverPhoneJs.bat
+++ BuildWeavverPhoneJs.bat
@@ -1,7 +1,8 @@
-java -jar google-closure-compiler\compiler.jar ^
-     --js=swfobject.js ^
-     --js=jquery-1.8.3.min.js ^
-     --js=jquery.tmpl.js ^
-     --js=jquery-ui-1.9.2.custom.min.js ^
-     --js=WeavverPhoneLib.js ^
-     --js_output_file=WeavverPhone.js
+mkdir build
+java -jar vendors\google-closure-compiler\compiler.jar ^
+     --js=src\swfobject.js ^
+     --js=src\jquery-1.8.3.min.js ^
+     --js=src\jquery.tmpl.js ^
+     --js=src\jquery-ui-1.9.2.custom.min.js ^
+     --js=src\WeavverPhoneLib.js ^
+     --js_output_file=build\WeavverPhone.js
